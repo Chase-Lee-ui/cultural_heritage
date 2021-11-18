0bb616f7d1bf2623d2c9247c98cea19b5f5863bd
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-11-17 20:42:21 -1000

    pushing dialog changes

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -18,12 +18,13 @@ public class DialogManager : MonoBehaviour
     private bool EnteredScene;
     private float JourneyTime = 1.0f;
     private float StartTime;
-    // public GameObject LookAtObject;
+    public GameObject LookAtObject;
+    private bool Panning;
+    public SpriteRenderer TextBubble;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.StartTime = Time.time;
         this.Dialog.text = string.Empty;
         StartDialogue();
     }
@@ -34,9 +35,23 @@ public class DialogManager : MonoBehaviour
         //slerping camera to look at dialog
         if(EnteredScene)
         {
-            if(Camera.Follow.position != this.LookAtTransform.position)
+            if(this.Camera.Follow.position != this.LookAtTransform.position && !this.Panning)
             {
-                this.Camera.Follow.position = Vector3.Slerp(this.CurrentPosition.position, this.LookAtTransform.position, Time.deltaTime);
+                float fracComplete = (Time.time - this.StartTime) / JourneyTime;
+                this.LookAtObject.transform.position = Vector3.Slerp(this.CurrentPosition.position, this.LookAtTransform.position, fracComplete);
+            }
+
+            if(this.Camera.Follow.position != this.CurrentPosition.position && this.Panning)
+            {
+                float fracComplete = (Time.time - this.StartTime) / JourneyTime;
+                this.LookAtObject.transform.position = Vector3.Slerp(this.LookAtTransform.position, this.CurrentPosition.position, fracComplete);
+                var Alpha = Mathf.Abs(this.Camera.Follow.position.x - this.CurrentPosition.position.x);
+                if((Alpha <= 0.01) && this.Panning)
+                {
+                    this.Camera.Follow = this.Player.gameObject.transform;
+                    this.PlayerMovementScript.enabled = true;
+                    this.gameObject.transform.parent.gameObject.SetActive(false);
+                }
             }
         }
 
@@ -79,7 +94,8 @@ public class DialogManager : MonoBehaviour
         }
         else
         {
-            this.gameObject.SetActive(false);
+            this.StartTime = Time.time;
+            this.Panning = true;
         }
     }
 
@@ -87,6 +103,7 @@ public class DialogManager : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
+            this.TextBubble.enabled = true;
             this.EnteredScene = true;
             this.Player = collision.gameObject;
             //saves monke transform
@@ -94,9 +111,9 @@ public class DialogManager : MonoBehaviour
             //saves monke movement
             this.PlayerMovementScript = collision.gameObject.GetComponent<Player_Movement>();
             this.PlayerMovementScript.enabled = false;
-            // var NewTransform = new GameObject();
-            // NewTransform.position = this.CurrentPosition.position;
-            // this.Camera.Follow = NewTransform;
+            this.LookAtObject.transform.position = collision.gameObject.transform.position;
+            this.Camera.Follow = this.LookAtObject.transform;
+            this.StartTime = Time.time;
         }
     }
 }
