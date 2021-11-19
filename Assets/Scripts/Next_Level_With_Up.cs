ba1a29991950bbb74b4061b469789d519e3c32b8
Benny <trieu6@hawaii.edu>
2021-11-19 13:43:39 -1000

    Deleting a package that was not used and added walls

--- Assets/Scripts/Next_Level_With_Up.cs
+++ Assets/Scripts/Next_Level_With_Up.cs
@@ -6,26 +6,15 @@ using UnityEngine.SceneManagement;
 public class Next_Level_With_Up : MonoBehaviour
 {
     public string NextScene;
- //   public string monkey;
- //   public float forwardInput;
     private bool teleport;
-    public GameObject monke;
- //   private Vector3 offset = new Vector3(5, 5, 5);
- //   void Start()
- //   {
- //      this.monke = GameObject.FindWithTag("Player");
- //   }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.W) && teleport) {
             SceneManager.LoadScene(this.NextScene, LoadSceneMode.Single);
-        //    transform.position = monke.transform.position + offset;
         }
     }
     void OnTriggerEnter2D (Collider2D collision)
     {
- //       Rigidbody rb = monke.GetComponent<Rigidbody>();
- //       Player_Movement recognize = monke.GetComponent<Player_Movement>();
         if (collision.gameObject.CompareTag("Player"))
         {
             teleport = true;
@@ -33,8 +22,6 @@ public class Next_Level_With_Up : MonoBehaviour
     }
     void OnTriggerExit2D(Collider2D collision)
     {
-        //       Rigidbody rb = monke.GetComponent<Rigidbody>();
-        //       Player_Movement recognize = monke.GetComponent<Player_Movement>();
         if (collision.gameObject.CompareTag("Player"))
         {
             teleport = false;
