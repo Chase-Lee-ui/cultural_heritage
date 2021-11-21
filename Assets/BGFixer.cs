121c2438ca2258b5537a4415b75236ff7ec41461
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-11-21 09:48:23 -1000

    Revert "Merge branch 'max' into chase"
    This reverts commit 7a47290ff9f4d6f0c39fabe7960e06593caf6bb9, reversing
    changes made to 04feb1f13af49de510c1917da964819ffabfa5a3.

---
+++ Assets/BGFixer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGFixer : MonoBehaviour
+{
+    private bool touchingBG;
+    public float changeX;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "BG")
+        {
+            touchingBG = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        touchingBG = false;
+    }
+
+    private void Update()
+    {
+        if (touchingBG) {
+            Vector2 pos = new Vector2(changeX, transform.position.y);
+            this.transform.position = pos;
+        }
+    }
+
+}
---
+++ Assets/Scripts/RepeatingFloor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingFloor : MonoBehaviour
+{
+
+    public float speed;
+
+    public float endX;
+    public float startX;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= endX){
+            Vector3 pos = new Vector3(startX, transform.position.y, transform.position.z);
+            transform.position = pos;
+        }
+    }
+}
