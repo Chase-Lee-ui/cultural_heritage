1788a5c5a0ae2324c43ea94b4f616b97d816a0e9
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-11-21 02:20:03 -1000

    hayn lvl

---
+++ Assets/Scripts/Pig_Button_Mash.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig_Button_Mash : MonoBehaviour
+{
+    private int ButtonMash;
+    private bool Inside;
+    public int TotalToPress;
+    public GameObject ReleasePig;
+    public bool Finished;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ButtonMash = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && this.Inside)
+        {
+            this.ButtonMash += 1;
+            if(this.ButtonMash >= this.TotalToPress)
+            {
+                this.ReleasePig.SetActive(true);
+                this.Finished = true;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            this.Inside = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            this.Inside = false;
+        }
+    }
+}
