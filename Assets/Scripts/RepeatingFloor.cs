305bfd5d7c2589e3a9d82d9a7bf3a409aada7a1b
Kelsy <91763178+Kelpome@users.noreply.github.com>
2021-11-20 17:23:40 -1000

    Updated Levels

--- Assets/Scripts/RepeatingFloor.cs
+++ Assets/Scripts/RepeatingFloor.cs
@@ -12,10 +12,10 @@ public class RepeatingFloor : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
 
         if (transform.position.x <= endX){
-            Vector2 pos = new Vector2(startX, transform.position.y);
+            Vector3 pos = new Vector3(startX, transform.position.y, transform.position.z);
             transform.position = pos;
         }
     }
